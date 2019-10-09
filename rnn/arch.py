6616aeae9ae7861fe1a75e3f41a02ed582d0414d
bleyddyn <bleyddyn.aprhys@gmail.com>
2019-10-08 17:43:30 -0700

    Added back everything from upstream. Still have to remove the reward handling code.

--- rnn/arch.py
+++ rnn/arch.py
@@ -11,119 +11,133 @@ from keras.optimizers import Adam
 
 import tensorflow as tf
 
-Z_FACTOR = 1
-
-LEARNING_RATE = 0.001
-# MIN_LEARNING_RATE = 0.001
-# DECAY_RATE = 1.0
-
-BATCH_SIZE =32
-EPOCHS = 20
-
-def get_mixture_coef(y_pred, gaussian_mixtures, z_dim):
-    
-    d = gaussian_mixtures * z_dim
-    
-    rollout_length = K.shape(y_pred)[1]
-    
-    pi = y_pred[:,:,:d]
-    mu = y_pred[:,:,d:(2*d)]
-    log_sigma = y_pred[:,:,(2*d):(3*d)]
-    #discrete = y_pred[:,3*gaussian_mixtures:]
-    
-    pi = K.reshape(pi, [-1, rollout_length, gaussian_mixtures, z_dim])
-    mu = K.reshape(mu, [-1, rollout_length, gaussian_mixtures, z_dim])
-    log_sigma = K.reshape(log_sigma, [-1, rollout_length, gaussian_mixtures, z_dim])
-
-    pi = K.exp(pi) / K.sum(K.exp(pi), axis=2, keepdims=True)
-    sigma = K.exp(log_sigma)
-    
-    return pi, mu, sigma#, discrete
-
-
-def tf_normal(y_true, mu, sigma, pi, gaussian_mixtures, z_dim):
-
-    rollout_length = K.shape(y_true)[1]
-    y_true = K.tile(y_true,(1,1,gaussian_mixtures))
-    y_true = K.reshape(y_true, [-1, rollout_length, gaussian_mixtures,z_dim])
-
-    oneDivSqrtTwoPI = 1 / math.sqrt(2*math.pi)
-    result = y_true - mu
-#   result = K.permute_dimensions(result, [2,1,0])
-    result = result * (1 / (sigma + 1e-8))
-    result = -K.square(result)/2
-    result = K.exp(result) * (1/(sigma + 1e-8))*oneDivSqrtTwoPI
-    result = result * pi
-    result = K.sum(result, axis=2) #### sum over gaussians
-    #result = K.prod(result, axis=2) #### multiply over latent dims
-    return result
-
-
 class RNN():
-    def __init__(self, z_dim, action_dim, hidden_units=256, gaussian_mixtures=5):
+    def __init__(self, z_dim, action_dim, hidden_units=256, gaussian_mixtures=5, batch_size=32, epochs=20, learning_rate=0.001, optim="Adam"):
         self.z_dim = z_dim
         self.action_dim = action_dim
         self.hidden_units = hidden_units
         self.gaussian_mixtures = gaussian_mixtures
+        self.learning_rate = learning_rate
+        self.optim = optim
+        self.epochs = epochs
+        self.batch_size = batch_size
 
         self.models = self._build()
         self.model = self.models[0]
-        self.model.summary()
         self.forward = self.models[1]
+        self.model.summary()
 
-	def _build(self):
+    def _build(self):
+
         #### THE MODEL THAT WILL BE TRAINED
         rnn_x = Input(shape=(None, self.z_dim + self.action_dim))
         lstm = LSTM(self.hidden_units, return_sequences=True, return_state = True)
 
-        lstm_output, _ , _ = lstm(rnn_x)
-        mdn = Dense(self.gaussian_mixtures * (3*self.z_dim))(lstm_output) #+ discrete_dim
+        lstm_output_model, _ , _ = lstm(rnn_x)
+        mdn = Dense(self.gaussian_mixtures * (3*self.z_dim))
+
+        mdn_model = mdn(lstm_output_model)
+
+        model = Model(rnn_x, mdn_model)
 
         #### THE MODEL USED DURING PREDICTION
         state_input_h = Input(shape=(self.hidden_units,))
         state_input_c = Input(shape=(self.hidden_units,))
         state_inputs = [state_input_h, state_input_c]
-        _ , state_h, state_c = lstm(rnn_x, initial_state = [state_input_h, state_input_c])
+        lstm_output_forward , state_h, state_c = lstm(rnn_x, initial_state = [state_input_h, state_input_c])
 
+        mdn_forward = mdn(lstm_output_forward)
 
-        def make_rnn_r_loss( gmix, z_dim ):
-            def rnn_r_loss(y_true, y_pred):
+        forward = Model([rnn_x] + [state_input_h, state_input_c], [mdn_forward, state_h, state_c])
 
-                pi, mu, sigma = get_mixture_coef(y_pred, gmix, z_dim)
-            
-                result = tf_normal(y_true, mu, sigma, pi, gmix, z_dim)
-                
-                result = -K.log(result + 1e-8)
-                result = K.mean(result, axis = (1,2)) # mean over rollout length and z dim
+        #### LOSS FUNCTIONS
 
-                return result
-            return rnn_r_loss
-        r_loss = make_rnn_r_loss( self.gaussian_mixtures, self.z_dim )
+        def rnn_z_loss(y_true, y_pred):
 
-        def make_rnn_kl_loss( gmix, z_dim ):
-            def rnn_kl_loss(y_true, y_pred):
-                pi, mu, sigma = get_mixture_coef(y_pred, gmix, z_dim)
-                kl_loss = - 0.5 * K.mean(1 + K.log(K.square(sigma)) - K.square(mu) - K.square(sigma), axis = [1,2,3])
-                return kl_loss
-            return rnn_kl_loss
-        kl_loss = make_rnn_kl_loss( self.gaussian_mixtures, self.z_dim )
+            z_true, _ = self.get_responses(y_true, self.z_dim)
+
+            d = self.gaussian_mixtures * self.z_dim
+            z_pred = y_pred[:,:,:(3*d)]
+            z_pred = K.reshape(z_pred, [-1, self.gaussian_mixtures * 3])
+
+            log_pi, mu, log_sigma = self.get_mixture_coef(z_pred)
+
+            flat_z_true = K.reshape(z_true,[-1, 1])
+
+            z_loss = log_pi + self.tf_lognormal(flat_z_true, mu, log_sigma)
+            z_loss = -K.log(K.sum(K.exp(z_loss), 1, keepdims=True))
+
+            z_loss = K.mean(z_loss)
+
+            return z_loss
+
+        def rnn_rew_loss(y_true, y_pred):
+
+            z_true, rew_true = self.get_responses(y_true, self.z_dim)
+
+            d = self.gaussian_mixtures * self.z_dim
+            reward_pred = y_pred[:,:,-1]
+
+            rew_loss =  K.binary_crossentropy(rew_true, reward_pred, from_logits = True)
+
+            rew_loss = K.mean(rew_loss)
+
+            return rew_loss
 
         def rnn_loss(y_true, y_pred):
-            return r_loss(y_true, y_pred) #+ rnn_kl_loss(y_true, y_pred)
 
+            z_loss = rnn_z_loss(y_true, y_pred)
+            rew_loss = rnn_rew_loss(y_true, y_pred)
 
-        rnn.compile(loss=rnn_loss, optimizer='rmsprop', metrics = [r_loss, kl_loss])
+            return Z_FACTOR * z_loss + REWARD_FACTOR * rew_loss
 
-		log_pi, mu, log_sigma = tf.split(z_pred, 3, 1)
-		log_pi = log_pi - K.log(K.sum(K.exp(log_pi), axis = 1, keepdims = True)) # axis 1 is the mixture axis
+        opti = Adam(lr=self.learning_rate)
+        model.compile(loss=rnn_loss, optimizer=opti, metrics = [rnn_z_loss, rnn_rew_loss])
 
-		return log_pi, mu, log_sigma
+        return (rnn,forward)
 
+    def train(self, rnn_input, rnn_output, validation_split = 0.2):
 
-	def tf_lognormal(self, z_true, mu, log_sigma):
+        earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='auto')
+        callbacks_list = [earlystop]
 
-		logSqrtTwoPI = np.log(np.sqrt(2.0 * np.pi))
-		return -0.5 * ((z_true - mu) / K.exp(log_sigma)) ** 2 - log_sigma - logSqrtTwoPI
+        self.model.fit(rnn_input, rnn_output,
+            shuffle=True,
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            validation_split=validation_split,
+            callbacks=callbacks_list)
 
+        self.model.save_weights('./rnn/weights.h5')
 
+    def train_batch(self, rnn_input, rnn_output):
 
+        self.model.fit(rnn_input, rnn_output,
+                       shuffle=False,
+                       epochs=1,
+                       batch_size=len(rnn_input))
+
+    def set_weights(self, filepath):
+        self.model.load_weights(filepath)
+
+    def save_weights(self, filepath):
+        self.model.save_weights(filepath)
+
+    def get_responses(self, y_true, z_dim):
+
+        z_true = y_true[:,:,:z_dim]
+        rew_true = y_true[:,:,-1]
+
+        return z_true, rew_true
+
+    def get_mixture_coef(self, z_pred):
+
+        log_pi, mu, log_sigma = tf.split(z_pred, 3, 1)
+        log_pi = log_pi - K.log(K.sum(K.exp(log_pi), axis = 1, keepdims = True)) # axis 1 is the mixture axis
+
+        return log_pi, mu, log_sigma
+
+    def tf_lognormal(self, z_true, mu, log_sigma):
+
+        logSqrtTwoPI = np.log(np.sqrt(2.0 * np.pi))
+        return -0.5 * ((z_true - mu) / K.exp(log_sigma)) ** 2 - log_sigma - logSqrtTwoPI
